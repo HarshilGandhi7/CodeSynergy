39b346da6a8c9ae719d393317a49e5ac24af47df
HarshilGandhi7 <harshil.tejash2022@vitstudent.ac.in>
2025-03-10 19:27:25 +0530

    "Update Editor.jsx: fix typo in isUpdatingFromSocket useRef"

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -9,7 +9,7 @@ const Editor = ({ code, setCode, socketRef, roomId }) => {
   const [debouncedCode, setDebouncedCode] = useState(code);
   const [iframeKey, setIframeKey] = useState(0);
   const editorRef = useRef(null);
-  const isUpdatingFromSockenew t = useRef(false);
+  const isUpdatingFromSocket = useRef(false);
 
   // Handle Local Code Change
   const handleCodeChange = useCallback(
